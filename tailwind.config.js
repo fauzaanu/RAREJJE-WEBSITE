ba6dfa042907fc0b9a0f55715e0b6aa295afc51c
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2022-10-05 14:00:57 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'rry': '#FFD236',
+        'rrb': '#01B3B7',
+        'rrg': '#91AF36',
+        'our-bg': '#D9E6FF'
+      }
+    },
+  },
+  plugins: [],
+}
